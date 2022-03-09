cd4b5d09c1f424a363ae94c6936ace95f49adfd3
Aswin V <vaswin91@gmail.com>
2022-03-09 14:17:48 +0530

    BoxyHQSAMLStrategy implementation

---
+++ app/lib/boxyhq-saml.ts
@@ -0,0 +1,118 @@
+// We need to import from Remix Auth the type of the strategy verify callback
+import { SessionStorage } from "remix";
+import type { AuthenticateOptions, StrategyVerifyCallback } from "remix-auth";
+// We need to import the OAuth2Strategy, the verify params and the profile interfaces
+import type {
+  OAuth2Profile,
+  OAuth2StrategyVerifyParams,
+} from "remix-auth-oauth2";
+import { OAuth2Strategy } from "remix-auth-oauth2";
+
+// These are the custom options we need from the developer to use the strategy
+export interface BoxyHQSAMLStrategyOptions {
+  domain: string;
+  clientID: string;
+  clientSecret: string;
+  callbackURL: string;
+}
+
+// This interface declare what extra params we will get from BoxyHQSAML on the
+// verify callback
+export interface BoxyHQSAMLExtraParams
+  extends Record<string, string | number> {}
+
+// The BoxyHQSAMLProfile extends the OAuth2Profile with the extra params and mark
+// some of them as required
+export interface BoxyHQSAMLProfile extends OAuth2Profile {
+  id: string;
+  email: string;
+  firstName?: string;
+  lastName?: string;
+}
+
+// And we create our strategy extending the OAuth2Strategy, we also need to
+// pass the User as we did on the FormStrategy, we pass the Auth0Profile and the
+// extra params
+export class BoxyHQSAMLStrategy<User> extends OAuth2Strategy<
+  User,
+  BoxyHQSAMLProfile,
+  BoxyHQSAMLExtraParams
+> {
+  // The OAuth2Strategy already has a name but we override it to be specific of
+  // the service we are using
+  name = "boxyhq-saml";
+
+  private userInfoURL: string;
+
+  // We receive our custom options and our verify callback
+  constructor(
+    options: BoxyHQSAMLStrategyOptions,
+    // Here we type the verify callback as a StrategyVerifyCallback receiving
+    // the User type and the OAuth2StrategyVerifyParams with the BoxyHQSAMLProfile
+    // and the BoxyHQSAMLExtraParams
+    // This way, when using the strategy the verify function will receive as
+    // params an object with accessToken, refreshToken, extraParams and profile.
+    // The latest two matching the types of BoxyHQSAMLProfile and BoxyHQSAMLExtraParams.
+    verify: StrategyVerifyCallback<
+      User,
+      OAuth2StrategyVerifyParams<BoxyHQSAMLProfile, BoxyHQSAMLExtraParams>
+    >
+  ) {
+    // And we pass the options to the super constructor using our own options
+    // to generate them, this was we can ask less configuration to the developer
+    // using our strategy
+    super(
+      {
+        authorizationURL: `${options.domain}/api/oauth/authorize`,
+        tokenURL: `${options.domain}/api/oauth/token`,
+        clientID: "dummy",
+        clientSecret: "dummy",
+        callbackURL: options.callbackURL,
+      },
+      verify
+    );
+
+    this.userInfoURL = `${options.domain}/api/oauth/userinfo`;
+  }
+
+  async authenticate(
+    request: Request,
+    sessionStorage: SessionStorage,
+    options: AuthenticateOptions
+  ): Promise<User> {
+    if (options.context?.clientID && options.context?.clientSecret) {
+      this.clientID = options.context.clientID;
+      this.clientSecret = options.context.clientSecret;
+    }
+    return super.authenticate(request, sessionStorage, options);
+  }
+  // We override the protected authorizationParams method to return a new
+  // URLSearchParams with custom params we want to send to the authorizationURL.
+  // Here we add the scope so Auth0 can use it, you can pass any extra param
+  // you need to send to the authorizationURL here base on your provider.
+  protected authorizationParams(): URLSearchParams {
+    const urlSearchParams: Record<string, string> = {
+      provider: "saml",
+    };
+
+    return new URLSearchParams(urlSearchParams);
+  }
+
+  // We also override how to use the accessToken to get the profile of the user.
+  // Here we fetch a BoxyHQSAML specific URL, get the profile data, and build the
+  // object based on the BoxyHQSAMLProfile interface.
+  protected async userProfile(accessToken: string): Promise<BoxyHQSAMLProfile> {
+    let response = await fetch(this.userInfoURL, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+
+    let data: BoxyHQSAMLProfile = await response.json();
+
+    let profile: BoxyHQSAMLProfile = {
+      ...data,
+      provider: this.name,
+    };
+
+    return profile;
+  }
+}
