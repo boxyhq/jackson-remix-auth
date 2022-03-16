2d87a7ee747568e15135d87cba370059617e6e3c
Aswin V <vaswin91@gmail.com>
2022-03-16 15:42:36 +0530

    AccessDenied component

---
+++ app/components/AccessDenied.tsx
@@ -0,0 +1,18 @@
+import { Link } from "remix";
+
+export default function AccessDenied() {
+  return (
+    <>
+      <h1 className="text-primary mt-2 mb-4 text-3xl font-bold">
+        Access Denied
+      </h1>
+      <p>
+        You must be signed in to view this page. Click on{" "}
+        <Link to="/login" className="link">
+          Sign In
+        </Link>{" "}
+        begin the SAML login flow using SAML Jackson and NextAuth.js.
+      </p>
+    </>
+  );
+}
