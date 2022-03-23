b60eda840decb78974000c6c629f77df5d8e4778
Aswin V <vaswin91@gmail.com>
2022-03-23 21:03:24 +0530

    Fix redirect link in logout

--- app/routes/logout.ts
+++ app/routes/logout.ts
@@ -2,5 +2,5 @@ import type { LoaderFunction } from "remix";
 import { auth } from "~/auth.server";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  await auth.logout(request, { redirectTo: "/" });
+  await auth.logout(request, { redirectTo: "/login" });
 };
