affe190457c2274de85e830768c6e5d8ccba847f
Aswin V <vaswin91@gmail.com>
2022-10-06 22:20:32 +0530

    Rename app routes

--- app/routes/auth.sso.callback.tsx
+++ app/routes/auth.sso.callback.tsx
@@ -2,7 +2,7 @@ import type { LoaderFunction } from "@remix-run/node";
 import { auth } from "~/auth.server";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  return auth.authenticate("boxyhq-saml", request, {
+  return auth.authenticate("boxyhq-sso", request, {
     successRedirect: "/private",
     failureRedirect: "/login",
   });
